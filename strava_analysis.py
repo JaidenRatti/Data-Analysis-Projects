dc22f5025011e81c117d452a72a425398c5c477a
JaidenRatti <jaidenratti@gmail.com>
2022-06-20 23:10:16 -0400

    more edits

--- strava_analysis.py
+++ strava_analysis.py
@@ -16,6 +16,7 @@ from matplotlib import style
 from datetime import datetime
 import july
 from july.utils import date_range
+import statistics as stat
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
@@ -41,101 +42,96 @@ access_token = res.json()['access_token']
 #use refresh token to get new access token
 
 heading = {'Authorization': 'Bearer ' + access_token}
-param = {'per_page': 150, 'page':1}
+param = {'per_page': 200, 'page':1}
 my_fulldataset = requests.get(activities_link, headers=heading, params=param).json()
 
 all_activities = pandas.json_normalize(my_fulldataset)
 
-
-
-
-
 def viewdata(data):
     print(data.columns)
+    #see 'template'
     print(data.shape)
+    #dimensions
     print(data['type'].value_counts())
     #see distribution of bikes --> walks --> hikes
 
 #viewdata(all_activities)
 
-bike_data = []
-run_data = []
-hike_data = []
-walk_data = []
+#creating lists of specific activity types
+def create_activity_dataset(attribute, full_dataset):
+    activity_data = []
+    for i in range(0,len(full_dataset))[::-1]:
+        #oldest entries at top
+        if full_dataset[i]["type"]==attribute:
+            activity_data.append(full_dataset[i])
+    return activity_data
 
-def create_bike_dataset(full):
-    for i in range(0,len(full))[::-1]:
-    #oldest entries appear at top
-        if(full[i]["type"]=="Ride"):
-            bike_data.append(full[i])
-    return bike_data
+hike_list = create_activity_dataset("Hike",my_fulldataset)
+bike_list = create_activity_dataset("Ride",my_fulldataset)
+run_list = create_activity_dataset("Run",my_fulldataset)
+walk_list = create_activity_dataset("Walk",my_fulldataset)
 
-def create_run_dataset(full):
-    for i in range(0,len(full))[::-1]:
-        if(full[i]["type"]=="Run"):
-            run_data.append(full[i])
-    return run_data
 
-def create_hike_dataset(full):
-    for i in range(0,len(full))[::-1]:
-        if(full[i]["type"]=="Hike"):
-            hike_data.append(full[i])
-    return hike_data
+#analyzing bike data (for now... since biking is the only activity I do... going to make this to analyze any type later)
 
-def create_walk_dataset(full):
-    for i in range(0,len(full))[::-1]:
-        if(full[i]["type"]=="Walk"):
-            walk_data.append(full[i])
-    return walk_data
-
-#currently not optimized...ignore
-
-hike_list = create_hike_dataset(my_fulldataset)
-bike_list = create_bike_dataset(my_fulldataset)
-run_list = create_run_dataset(my_fulldataset)
-walk_list = create_walk_dataset(my_fulldataset)
-
-#analyzing bike data (for now.. going to make this to analyze any type later)
-
-def distances(bike_data):
-    bike_distances= []
+def unique_list_attribute(attribute, bike_data):
+    list_of_attribute = []
     for data in bike_data:
-        raw_distance = data["distance"]
-        real_distance = round(raw_distance/1000,2)
-        #convert from m-->km
-        bike_distances.append(real_distance)
-    return bike_distances
+        raw_data = data[attribute]
+        if attribute == "average_speed" or attribute == "max_speed":
+            real_data = round(raw_data*3.6,2)
+            #convert m/s-->km/h
+        if attribute =="distance":
+            real_data = round(raw_data/1000,2)
+            #convert m-->km
+        list_of_attribute.append(real_data)
+    return list_of_attribute
 
-def avg_speed(bike_data):
-    avg_bike_speeds = []
+bike_distance_list = unique_list_attribute("distance",bike_list)
+#creating list of just distances
+bike_avg_speed_list = unique_list_attribute("average_speed", bike_list)
+#^^
+bike_max_speed_list = unique_list_attribute("max_speed",bike_list)
+#^^
+
+def country(bike_data):
+    stationary_counter = 0
+    canada_counter = 0
+    country_counter = {}
     for data in bike_data:
-        raw_avg_bike_speed = data["average_speed"]
-        real_avg_bike_speed = round(raw_avg_bike_speed*3.6,2)
-        #convert from m/s to km/h
-        avg_bike_speeds.append(real_avg_bike_speed)
-    return avg_bike_speeds
+        print(data["location_country"])
 
-def max_speed(bike_data):
-    max_bike_speeds = []
-    for data in bike_data:
-        raw_max_bike_speed = data["max_speed"]
-        real_max_bike_speed = round(raw_max_bike_speed*3.6,2)
-        #convert from m/s to km/h
-        max_bike_speeds.append(real_max_bike_speed)
-    return max_bike_speeds
+#Issue with Strava API (displaying all location_country data has only home country and not country the activity actually took place in :( )
+#will come back to this
 
-bike_distance_list = distances(bike_list)
-bike_avg_speed_list = avg_speed(bike_list)
-bike_max_speed_list = max_speed(bike_list)
-#print(bike_avg_speed_list)
-#print(bike_max_speed_list)
-highest_speed = max(bike_max_speed_list)
-print("Highest Speed Recorded is " +str(highest_speed)+ "km/h")
+def get_distance_of_instance(list_of_data, distances):
+    highest = max(list_of_data)
+    index_highest = list_of_data.index(highest)
+    instance_highest = (distances[index_highest])
+    return highest, instance_highest
 
-dates = date_range("2020-01-01","2020-12-31")
-data = np.random.randint(0,14,len(dates))
+max_speed_distance = get_distance_of_instance(bike_max_speed_list, bike_distance_list)
+#"How far (km) was the ride where I got max speed?"
+max_avg_speed_distance = get_distance_of_instance(bike_avg_speed_list, bike_distance_list)
+#"How far (km) was the ride where I got my highest average speed?"
+max_distance = get_distance_of_instance(bike_distance_list, bike_distance_list)
+#"How far was my furthest ride!"
+
+print("Highest Speed Recorded is " +str(max_speed_distance[0])+ "km/h on a " + str(max_speed_distance[1])+ "km ride")
+print("Highest Average Speed Recorded is " +str(max_avg_speed_distance[0])+"km/h on a " + str(max_avg_speed_distance[1])+"km ride")
+print("Furthest Distance Recorded is " +str(max_distance[0]) +"km" )
+#displaying the variables above
+
+proper = []
+for i in range(0,len(bike_list)):
+    size = len(bike_list[i]["start_date"])
+    proper.append(bike_list[i]["start_date"][:size-10])
+
+dates = date_range(proper[0],proper[-1])
+data = bike_distance_list
 july.heatmap(dates, data, title='Bike Activity', cmap="github")
-
+july.calendar_plot(dates, data, title = 'Bike Activity',cmap='github')
+#play around more with this
 
 style.use('fast')
 
@@ -170,4 +166,24 @@ def animate2(distances,avgspeed):
     plt.ylabel('Average Speed (km/h)')
 an = animation.FuncAnimation(figure2, animate2(bike_distance_list, bike_avg_speed_list))
 
+figure3 = plt.figure("Figure 3")
+
+def animate3(distances):
+    bins = [0,5,10,20,30,40,50,60,70]
+    median_distance = stat.median(distances)
+    mean_distance = round(stat.mean(distances),2)
+    mode_distance = stat.mode(distances)
+    color = "#fc4c02"
+    color2 = "#fc4c03"
+    color3 = "#fc4c04"
+    plt.axvline(median_distance, color=color, label='Median Distance (' +str(median_distance)+')')
+    plt.axvline(mean_distance, color=color2, label='Mean Distance ('+str(mean_distance)+')')
+    plt.axvline(mode_distance, color=color3, label='Mode Distance ('+str(mode_distance)+")")
+    plt.hist(distances, bins=bins, edgecolor = 'black')
+    plt.legend()
+    plt.title('Distance by Count of Rides')
+    plt.xlabel('Distance (km)')
+    plt.ylabel('Frequency')
+
+a = animation.FuncAnimation(figure3,animate3(bike_distance_list))
 plt.show()
